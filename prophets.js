c032da672fb5b9e35f02003283ffde2144f8a761
Martin Moraga <moragamartin2@gmail.com>
2026-07-06 17:41:24 -0300

    prophets activity

---
+++ prophets.js
@@ -0,0 +1,34 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.prophets);
+    displayProphets(data.prophets);
+}
+getProphetData();
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section')
+        let fullName = document.createElement('h2')
+        let birthDate = document.createElement('p')
+        let placeOfBirth = document.createElement('p');
+        let portrait = document.createElement('img')
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        placeOfBirth.textContent = `Place of Birth: ${prophet.birthplace}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy')
+        portrait.setAttribute('width', '200px');
+        portrait.setAttribute('height', 'auto');
+        card.appendChild(fullName)
+        card.appendChild(birthDate)
+        card.appendChild(placeOfBirth)
+        card.appendChild(portrait)
+        cards.appendChild(card)
+    });
+}
